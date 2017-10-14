b2b165768f0ef0daf23425ce7e25a271c6015825
Sajjad <abdolmaleki.s@gmail.com>
2017-10-14 10:37:41 +0330

    add printer  codes and capsulation

--- app/src/main/java/com/technotapp/servicestation/activity/MainActivity.java
+++ app/src/main/java/com/technotapp/servicestation/activity/MainActivity.java
@@ -3,8 +3,6 @@ package com.technotapp.servicestation.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.technotapp.servicestation.R;
 import com.technotapp.servicestation.pax.mag.IMagCardCallback;
@@ -21,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void run(View v) {
         MagCard magCard = MagCard.getInstance();
+
         magCard.start(this, new IMagCardCallback() {
             @Override
             public void onFail() {
@@ -28,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSuccessful(String track1, String track2, String track3) {
+
             }
         });
     }
--- app/src/main/java/com/technotapp/servicestation/application/Constant.java
+++ app/src/main/java/com/technotapp/servicestation/application/Constant.java
@@ -2,4 +2,8 @@ package com.technotapp.servicestation.application;
 
 public class Constant {
 
+    static class Pax {
+        public static final String TEST = "test";
+    }
+
 }
