7971a17b444d0d1ad55309f6bf5bb8b0a0a9863a
Kourosh Mousavi <mousavi.card@gmail.com>
2017-10-23 08:27:36 +0330

    - fix bug 4

--- app/src/main/java/com/technotapp/servicestation/activity/CardServiceActivity.java
+++ app/src/main/java/com/technotapp/servicestation/activity/CardServiceActivity.java
@@ -1,25 +1,15 @@
 package com.technotapp.servicestation.activity;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.ImageButton;
-
 import com.technotapp.servicestation.Infrastructure.AppMonitor;
 import com.technotapp.servicestation.R;
-import com.technotapp.servicestation.adapter.DataModel.SubMenuModel;
-import com.technotapp.servicestation.adapter.SubMenuAdapter;
-import com.technotapp.servicestation.fragment.CardServiceBalanceFragment;
 import com.technotapp.servicestation.fragment.CardServiceFragment;
 
-import java.util.ArrayList;
-
-public class CardServiceActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
+public class CardServiceActivity extends AppCompatActivity implements  View.OnClickListener {
 //    ImageButton btnBack;
 
     @Override
@@ -39,8 +29,6 @@ public class CardServiceActivity extends AppCompatActivity implements AdapterVie
 
     private void submitFragment() {
         try {
-
-
             CardServiceFragment fragment = CardServiceFragment.newInstance();
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -51,7 +39,6 @@ public class CardServiceActivity extends AppCompatActivity implements AdapterVie
     }
 
     private void bindView() {
-//
 //        btnBack.setOnClickListener(this);
 //        btnBack = (ImageButton) findViewById(R.id.activity_card_service_back);
 
@@ -70,23 +57,6 @@ public class CardServiceActivity extends AppCompatActivity implements AdapterVie
     }
 
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (position) {
-            case 3:
-                startActivity(new Intent(CardServiceActivity.this, CardServiceBalanceFragment.class));
-                break;
-//            case 1:
-//                startActivity(new Intent(CardServiceActivity.this, .class));
-//                break;
-//            case 2:
-//                startActivity(new Intent(CardServiceActivity.this, .class));
-//                break;
-//            case 3:
-//                startActivity(new Intent(CardServiceActivity.this, .class));
-//                break;
-        }
-    }
 
     @Override
     public void onClick(View v) {
