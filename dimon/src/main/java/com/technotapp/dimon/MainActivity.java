9db73fa797772ab637996e07776bd45a2378e72b
Sajjad <abdolmaleki.s@gmail.com>
2017-10-29 15:10:06 +0330

    - create app lunch meth in dimon

--- dimon/src/main/java/com/technotapp/dimon/MainActivity.java
+++ dimon/src/main/java/com/technotapp/dimon/MainActivity.java
@@ -1,7 +1,9 @@
 package com.technotapp.dimon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            lunchMainApp();
+
+        } catch (Exception e) {
+            Log.d("ex", e.getMessage());
+        }
+
+    }
+
+    private void lunchMainApp() {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.technotapp.servicestation");
+        if (launchIntent != null) {
+            startActivity(launchIntent);//null pointer check in case package name was not found
+        }
+    }
+
 }
