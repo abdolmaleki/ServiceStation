e26488aafc68c4ecce91543ee37550cf81376d57
Sajjad Abdolmaleki <abdolmaleki.s@gmail.com>
2018-02-14 14:05:21 +0330

    - change transaction to api-base

--- app/src/main/java/com/technotapp/servicestation/activity/BaseActivity.java
+++ app/src/main/java/com/technotapp/servicestation/activity/BaseActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-
-import com.technotapp.servicestation.Infrastructure.PaxHelper;
 import com.technotapp.servicestation.setting.Session;
 
 
